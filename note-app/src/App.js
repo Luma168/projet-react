d42ba5f0c784bee23c7850f127c7bee944c890bf
Mohamed Adam Ben Nejma <ademben25@outlook.com>
2024-01-25 13:49:08 +0100

    added darkmode

--- note-app/src/App.js
+++ note-app/src/App.js
@@ -1,18 +1,31 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import './App.css';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { useState } from 'react';
 
 export default function App() {
+  const [themeVal, setThemeVal] = useState(true)
+
+  const theme = createTheme({
+    palette:{
+      mode: themeVal ? 'dark' : 'light'
+    }
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Navigate to={"/notes"} />} />
-        
-        <Route path="/notes" element={<Home />}>
-          <Route path=":currentNoteId" element={<Home />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Navigate to={"/notes"} />} />
+          
+          <Route path="/notes" element={<Home onChangeTheme={setThemeVal} />}>
+            <Route path=":currentNoteId" element={<Home onChangeTheme={setThemeVal}  />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
